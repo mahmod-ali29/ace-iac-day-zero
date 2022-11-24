06943b8acfdafa3f2b391b9b63895e161931cfa5
mahmod-ali29 <43117883+mahmod-ali29@users.noreply.github.com>
2022-11-24 13:10:48 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "<replace-with-your-Terraform-Cloud-organization-and-uncomment>"
+    organization = "aviatrix-iac12312"
     workspaces {
       name = "ace-iac-day-zero"
     }
